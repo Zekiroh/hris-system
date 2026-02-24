c49163fd295b8f49367b39c795e8076eab98f84c
Zekiroh <jheraldvillafuerte28@gmail.com>
2026-02-24 18:58:16 +0800

    employee-core: validate employees list query params

--- apps/api/HRIS.Api/Features/Employees/DTOs/GetEmployeesQuery.cs
+++ apps/api/HRIS.Api/Features/Employees/DTOs/GetEmployeesQuery.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HRIS.Api.Features.Employees.DTOs;
 
 public class GetEmployeesQuery
 {
     // Pagination
+
+    [Range(1, int.MaxValue)]
     public int Page { get; set; } = 1;
+
+    [Range(1, 100)]
     public int PageSize { get; set; } = 10;
 
     // Search
+
+    [MaxLength(100)]
     public string? Search { get; set; }
 
     // Filters
+
     public bool? IsActive { get; set; }
 }
