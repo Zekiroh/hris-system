a7e06286cc8987e31fc59bfb871462df52415b48
Joenas18ww <jonaseisenmanguiat@gmail.com>
2026-06-01 13:11:45 +0800

    feat: reset all fields after submit

--- apps/web/src/pages/DailyReport/DailyAccomplishmentReport.tsx
+++ apps/web/src/pages/DailyReport/DailyAccomplishmentReport.tsx
@@ -349,6 +349,40 @@ export default function DailyAccomplishmentReport() {
     localStorage.setItem("dar_submissions", JSON.stringify([sub, ...subs]));
     setShowConfirm(false);
     setShowSuccess(true);
+
+    // Reset all fields
+    setDevName("");
+    setDate(today);
+    setWorkArr("On-site");
+    setProject("");
+    setSprint("");
+    setTeam("");
+    setSubmittedTo("");
+    setTimeIn("08:00");
+    setTimeOut("17:00");
+    setBreakMins(60);
+    setSubTime("");
+    setStandup("Yes");
+    setReachable("Yes");
+    setAvgResponse("");
+    setConnIssues("");
+    setCollabLog("");
+    setTasks([createEmptyTask(1)]);
+    setDevHrs("");
+    setMeetingHrs("");
+    setIdleHrs("");
+    setKeyAccomp("");
+    setBlockers("");
+    setRisks("");
+    setPlanTmr("");
+    setEscalation("");
+    setChecklist(Array(6).fill(false));
+    setTmrArr("On-site");
+    setTmrTimeIn("08:00");
+    setLeaveNotice("");
+    setPreparedBy("");
+    setPreparedSig("");
+    setDateSubmitted(today);
   };
 
   const checklistItems = [
