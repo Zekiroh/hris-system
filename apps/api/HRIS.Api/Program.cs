71317d36159231b31fe6e2337260b649d59b5707
Joenas18ww <jonaseisenmanguiat@gmail.com>
2026-06-05 15:39:16 +0800

    resolve: merge conflicts on Program.cs and appsettings

--- apps/api/HRIS.Api/Program.cs
+++ apps/api/HRIS.Api/Program.cs
@@ -72,7 +72,7 @@ var connectionString = builder.Configuration.GetConnectionString("Default");
 if (string.IsNullOrWhiteSpace(connectionString))
     throw new InvalidOperationException("ConnectionStrings:Default is missing. Set it via user-secrets.");
 
-var serverVersion = new MySqlServerVersion(new Version(8, 0, 45));
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 0));
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseMySql(connectionString, serverVersion);
