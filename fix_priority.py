9d8f0cfc73bee1095bae29d8b31d8376d3646c5f
Joenas18ww <jonaseisenmanguiat@gmail.com>
2026-06-11 20:15:15 +0800

    chore: remove patch scripts and bak files

--- fix_priority.py
+++
@@ -1,9 +0,0 @@
-﻿f = 'apps/web/src/pages/DailyReport/DailyAccomplishmentReport.tsx'
-lines = open(f, encoding='utf-8').readlines()
-for i, l in enumerate(lines):
-    if 'width: "120px"' in l and i < 450:
-        lines[i] = l.replace('width: "120px"', 'width: "140px"')
-        print(f"Fixed line {i+1}")
-        break
-open(f, 'w', encoding='utf-8').writelines(lines)
-print('Done!')
